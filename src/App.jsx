36ce69e2caf17dbabd87a2539ba1f120798be4d0
chinmay <jain.chinmay13@gmail.com>
2023-10-05 14:52:38 +0530

    first change by chinmay

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore';
+
+function App() {
+  const auth = getAuth();
+  const db = getFirestore();
+  const googleProvider = new GoogleAuthProvider();
+
+  const [user, setUser] = useState(null);
+  const [data, setData] = useState([]);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const fetchData = async () => {
+    const usersCollection = collection(db, 'users');
+    const querySnapshot = await getDocs(usersCollection);
+
+    const userData = [];
+    querySnapshot.forEach((doc) => {
+      userData.push({ id: doc.id, ...doc.data() });
+    });
+
+    setData(userData);
+  };
+
+  useEffect(() => {
+    // Listen for changes in the user's authentication state
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    // Retrieve data from Firestore
+    fetchData();
+  }, [auth, db]);
+
+  const handleSignUp = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSignIn = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSignInWithPopup = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider); 
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="App">
+      {user ? (
+        <>
+          <p>Welcome, {user.email}!</p>
+          <button onClick={handleSignOut}>Sign Out</button>
+          <ul>
+            {data.map((item) => (
+              <li key={item.id}>{item.name} ({item.email})</li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleSignUp}>Sign Up</button>
+          <button onClick={handleSignIn}>Sign In</button>
+          <button onClick={handleSignInWithPopup}>Sign In with Google</button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
